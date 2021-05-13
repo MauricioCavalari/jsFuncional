06097ec12f6d4593c478bed43f03929add56bc71
MauricioCavalari <cavalarimauricio@gmail.com>
2021-05-13 20:49:42 -0300

    First Class Function

---
+++ funcional/first_class_function.js
@@ -0,0 +1,9 @@
+// Diz-se que uma linguagem de programação possui
+// funções de primeira classe quando funções nessa
+// linguagem são tratadas como qualquer outra váriavel. 
+
+const x = 3 
+const y = txt => `Esse é o texto: ${txt}`
+
+console.log(x)
+console.log(y('Olá'))
